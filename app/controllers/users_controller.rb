8410f8fbf52100ec2905f41e8fd52110ad2ef88c
Ricardo <ry.yrupailla@gmail.com>
2019-04-09 19:48:06 -0500

    Add last name refactorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.valid?
-      @user.save
+      @user.write_attribute(:last_name, last_name_refactor(@user[:last_name]))
+      @user.save      
       redirect_to "/users", status: 301
     else
-      errors = @user.errors.full_messages
+      errors = @user.errors.full_messages      
       redirect_to "/users/new", status: 301, flash: {errors: errors}
     end
 
@@ -33,6 +34,28 @@ class UsersController < ApplicationController
       :password_confirmation,
       :password_hint
     )
+  end
+
+  def last_name_refactor(last_name)
+    last_name_array = last_name.downcase.split(' ')  
+
+    articles = [
+      "de",
+      "del",
+      "la",
+      "las",
+      "los"
+    ]
+
+    last_name_array.map! do |word|
+      if articles.include? word 
+        word
+      else
+        word.capitalize
+      end
+    end
+
+    last_name_array.join(' ')
   end
 
 end
